c83610775e8f21ad06b709ea86a51e170bdce770
Yonatan Sasson <107778824+Yuni-sa@users.noreply.github.com>
2023-08-27 09:54:07 +0300

    Support finding ingresses (#31)
    * support finding ingresses
    
    * support ingresses with defaultBackend
    
    * remove unnecessary check for service backend in retrieveUsedIngress function
    
    the condition 'if path.Backend.Service != nil' was causing resources to be skipped
    incorrectly.

---
+++ cmd/kor/ingresses.go
@@ -0,0 +1,25 @@
+package kor
+
+import (
+	"github.com/spf13/cobra"
+	"github.com/yonahd/kor/pkg/kor"
+)
+
+var ingressCmd = &cobra.Command{
+	Use:     "ingress",
+	Aliases: []string{"ing"},
+	Short:   "Gets unused ingresses",
+	Args:    cobra.ExactArgs(0),
+	Run: func(cmd *cobra.Command, args []string) {
+		if outputFormat == "json" {
+			kor.GetUnusedIngressesJSON(namespace, kubeconfig)
+		} else {
+			kor.GetUnusedIngresses(namespace, kubeconfig)
+		}
+
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(ingressCmd)
+}
