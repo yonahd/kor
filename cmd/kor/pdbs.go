0be0ff0300e6d77dd99963d00dbf98970b2930ba
Yonatan Sasson <yonatanxd7@gmail.com>
2023-08-31 15:36:52 +0300

    support json output for Pdbs

--- cmd/kor/pdbs.go
+++ cmd/kor/pdbs.go
@@ -1,6 +1,8 @@
 package kor
 
 import (
+	"fmt"
+
 	"github.com/spf13/cobra"
 	"github.com/yonahd/kor/pkg/kor"
 )
@@ -11,7 +13,11 @@ var pdbCmd = &cobra.Command{
 	Args:  cobra.NoArgs,
 	Run: func(cmd *cobra.Command, args []string) {
 		if outputFormat == "json" {
-			kor.GetUnusedPdbsJson(namespace, kubeconfig)
+			if jsonResponse, err := kor.GetUnusedPdbsJson(namespace, kubeconfig); err != nil {
+				fmt.Println(err)
+			} else {
+				fmt.Println(jsonResponse)
+			}
 		} else {
 			kor.GetUnusedPdbs(namespace, kubeconfig)
 		}
