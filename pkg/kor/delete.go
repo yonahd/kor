e20bb1a34428e0a766ad1688719579db0b5858e1
Patrick Almeida <patricktalmeida@gmail.com>
2023-11-15 09:25:20 -0400

    feat: add inUse label prompt for non deleted resources

--- pkg/kor/delete.go
+++ pkg/kor/delete.go
@@ -4,8 +4,15 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"reflect"
 	"strings"
 
+	appsv1 "k8s.io/api/apps/v1"
+	autoscalingv1 "k8s.io/api/autoscaling/v1"
+	corev1 "k8s.io/api/core/v1"
+	networkingv1beta1 "k8s.io/api/networking/v1beta1"
+	policyv1beta1 "k8s.io/api/policy/v1beta1"
+	rbacv1 "k8s.io/api/rbac/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 )
@@ -51,164 +58,81 @@ func DeleteResourceCmd() map[string]func(clientset kubernetes.Interface, namespa
 }
 
 func FlagResource(clientset kubernetes.Interface, namespace, resourceType, resourceName string) error {
-	var flagResourceApiMap = map[string]func(clientset kubernetes.Interface, namespace, resourceName string) error{
-		"ConfigMap": func(clientset kubernetes.Interface, namespace, resourceName string) error {
-			configMap, err := clientset.CoreV1().ConfigMaps(namespace).Get(context.TODO(), resourceName, metav1.GetOptions{})
-			if err != nil {
-				return err
-			}
-
-			if configMap.Labels == nil {
-				configMap.Labels = make(map[string]string)
-			}
-
-			configMap.Labels["kor/used"] = "true"
-			_, err = clientset.CoreV1().ConfigMaps(namespace).Update(context.TODO(), configMap, metav1.UpdateOptions{})
-			return err
-		},
-		"Secret": func(clientset kubernetes.Interface, namespace, resourceName string) error {
-			secret, err := clientset.CoreV1().Secrets(namespace).Get(context.TODO(), resourceName, metav1.GetOptions{})
-			if err != nil {
-				return err
-			}
-
-			if secret.Labels == nil {
-				secret.Labels = make(map[string]string)
-			}
-
-			secret.Labels["kor/used"] = "true"
-			_, err = clientset.CoreV1().Secrets(namespace).Update(context.TODO(), secret, metav1.UpdateOptions{})
-			return err
-		},
-		"Service": func(clientset kubernetes.Interface, namespace, resourceName string) error {
-			service, err := clientset.CoreV1().Services(namespace).Get(context.TODO(), resourceName, metav1.GetOptions{})
-			if err != nil {
-				return err
-			}
-
-			if service.Labels == nil {
-				service.Labels = make(map[string]string)
-			}
-
-			service.Labels["kor/used"] = "true"
-			_, err = clientset.CoreV1().Services(namespace).Update(context.TODO(), service, metav1.UpdateOptions{})
-			return err
-		},
-		"Deployment": func(clientset kubernetes.Interface, namespace, resourceName string) error {
-			deployment, err := clientset.AppsV1().Deployments(namespace).Get(context.TODO(), resourceName, metav1.GetOptions{})
-			if err != nil {
-				return err
-			}
-
-			if deployment.Labels == nil {
-				deployment.Labels = make(map[string]string)
-			}
-
-			deployment.Labels["kor/used"] = "true"
-			_, err = clientset.AppsV1().Deployments(namespace).Update(context.TODO(), deployment, metav1.UpdateOptions{})
-			return err
-		},
-		"HPA": func(clientset kubernetes.Interface, namespace, resourceName string) error {
-			hpa, err := clientset.AutoscalingV1().HorizontalPodAutoscalers(namespace).Get(context.TODO(), resourceName, metav1.GetOptions{})
-			if err != nil {
-				return err
-			}
-
-			if hpa.Labels == nil {
-				hpa.Labels = make(map[string]string)
-			}
-
-			hpa.Labels["kor/used"] = "true"
-			_, err = clientset.AutoscalingV1().HorizontalPodAutoscalers(namespace).Update(context.TODO(), hpa, metav1.UpdateOptions{})
-			return err
-		},
-		"Ingress": func(clientset kubernetes.Interface, namespace, resourceName string) error {
-			ingress, err := clientset.NetworkingV1beta1().Ingresses(namespace).Get(context.TODO(), resourceName, metav1.GetOptions{})
-			if err != nil {
-				return err
-			}
-
-			if ingress.Labels == nil {
-				ingress.Labels = make(map[string]string)
-			}
-
-			ingress.Labels["kor/used"] = "true"
-			_, err = clientset.NetworkingV1beta1().Ingresses(namespace).Update(context.TODO(), ingress, metav1.UpdateOptions{})
-			return err
-		},
-		"PDB": func(clientset kubernetes.Interface, namespace, resourceName string) error {
-			pdb, err := clientset.PolicyV1beta1().PodDisruptionBudgets(namespace).Get(context.TODO(), resourceName, metav1.GetOptions{})
-			if err != nil {
-				return err
-			}
-
-			if pdb.Labels == nil {
-				pdb.Labels = make(map[string]string)
-			}
-
-			pdb.Labels["kor/used"] = "true"
-			_, err = clientset.PolicyV1beta1().PodDisruptionBudgets(namespace).Update(context.TODO(), pdb, metav1.UpdateOptions{})
-			return err
-		},
-		"Roles": func(clientset kubernetes.Interface, namespace, resourceName string) error {
-			role, err := clientset.RbacV1().Roles(namespace).Get(context.TODO(), resourceName, metav1.GetOptions{})
-			if err != nil {
-				return err
-			}
-
-			if role.Labels == nil {
-				role.Labels = make(map[string]string)
-			}
-
-			role.Labels["kor/used"] = "true"
-			_, err = clientset.RbacV1().Roles(namespace).Update(context.TODO(), role, metav1.UpdateOptions{})
-			return err
-		},
-		"PVC": func(clientset kubernetes.Interface, namespace, resourceName string) error {
-			pvc, err := clientset.CoreV1().PersistentVolumeClaims(namespace).Get(context.TODO(), resourceName, metav1.GetOptions{})
-			if err != nil {
-				return err
-			}
-
-			if pvc.Labels == nil {
-				pvc.Labels = make(map[string]string)
-			}
-
-			pvc.Labels["kor/used"] = "true"
-			_, err = clientset.CoreV1().PersistentVolumeClaims(namespace).Update(context.TODO(), pvc, metav1.UpdateOptions{})
-			return err
-		},
-		"StatefulSet": func(clientset kubernetes.Interface, namespace, resourceName string) error {
-			statefulSet, err := clientset.AppsV1().StatefulSets(namespace).Get(context.TODO(), resourceName, metav1.GetOptions{})
-			if err != nil {
-				return err
-			}
-
-			if statefulSet.Labels == nil {
-				statefulSet.Labels = make(map[string]string)
-			}
-
-			statefulSet.Labels["kor/used"] = "true"
-			_, err = clientset.AppsV1().StatefulSets(namespace).Update(context.TODO(), statefulSet, metav1.UpdateOptions{})
-			return err
-		},
-		"ServiceAccount": func(clientset kubernetes.Interface, namespace, resourceName string) error {
-			serviceAccount, err := clientset.CoreV1().ServiceAccounts(namespace).Get(context.TODO(), resourceName, metav1.GetOptions{})
-			if err != nil {
-				return err
-			}
-
-			if serviceAccount.Labels == nil {
-				serviceAccount.Labels = make(map[string]string)
-			}
-
-			serviceAccount.Labels["kor/used"] = "true"
-			_, err = clientset.CoreV1().ServiceAccounts(namespace).Update(context.TODO(), serviceAccount, metav1.UpdateOptions{})
-			return err
-		},
+	resource, err := getResource(clientset, namespace, resourceType, resourceName)
+	if err != nil {
+		return err
 	}
 
-	return flagResourceApiMap[resourceType](clientset, namespace, resourceName)
+	labelField := reflect.ValueOf(resource).Elem().FieldByName("Labels")
+	if labelField.IsValid() {
+		labels := labelField.Interface().(map[string]string)
+		if labels == nil {
+			labels = make(map[string]string)
+		}
+		labels["kor/used"] = "true"
+		labelField.Set(reflect.ValueOf(labels))
+	} else {
+		return fmt.Errorf("unable to set labels for resource type: %s", resourceType)
+	}
+
+	_, err = updateResource(clientset, namespace, resourceType, resource)
+	return err
+}
+
+func updateResource(clientset kubernetes.Interface, namespace, resourceType string, resource interface{}) (interface{}, error) {
+	switch resourceType {
+	case "ConfigMap":
+		return clientset.CoreV1().ConfigMaps(namespace).Update(context.TODO(), resource.(*corev1.ConfigMap), metav1.UpdateOptions{})
+	case "Secret":
+		return clientset.CoreV1().Secrets(namespace).Update(context.TODO(), resource.(*corev1.Secret), metav1.UpdateOptions{})
+	case "Service":
+		return clientset.CoreV1().Services(namespace).Update(context.TODO(), resource.(*corev1.Service), metav1.UpdateOptions{})
+	case "Deployment":
+		return clientset.AppsV1().Deployments(namespace).Update(context.TODO(), resource.(*appsv1.Deployment), metav1.UpdateOptions{})
+	case "HPA":
+		return clientset.AutoscalingV1().HorizontalPodAutoscalers(namespace).Update(context.TODO(), resource.(*autoscalingv1.HorizontalPodAutoscaler), metav1.UpdateOptions{})
+	case "Ingress":
+		return clientset.NetworkingV1beta1().Ingresses(namespace).Update(context.TODO(), resource.(*networkingv1beta1.Ingress), metav1.UpdateOptions{})
+	case "PDB":
+		return clientset.PolicyV1beta1().PodDisruptionBudgets(namespace).Update(context.TODO(), resource.(*policyv1beta1.PodDisruptionBudget), metav1.UpdateOptions{})
+	case "Roles":
+		return clientset.RbacV1().Roles(namespace).Update(context.TODO(), resource.(*rbacv1.Role), metav1.UpdateOptions{})
+	case "PVC":
+		return clientset.CoreV1().PersistentVolumeClaims(namespace).Update(context.TODO(), resource.(*corev1.PersistentVolumeClaim), metav1.UpdateOptions{})
+	case "StatefulSet":
+		return clientset.AppsV1().StatefulSets(namespace).Update(context.TODO(), resource.(*appsv1.StatefulSet), metav1.UpdateOptions{})
+	case "ServiceAccount":
+		return clientset.CoreV1().ServiceAccounts(namespace).Update(context.TODO(), resource.(*corev1.ServiceAccount), metav1.UpdateOptions{})
+	}
+	return nil, fmt.Errorf("resource type '%s' is not supported", resourceType)
+}
+
+func getResource(clientset kubernetes.Interface, namespace, resourceType, resourceName string) (interface{}, error) {
+	switch resourceType {
+	case "ConfigMap":
+		return clientset.CoreV1().ConfigMaps(namespace).Get(context.TODO(), resourceName, metav1.GetOptions{})
+	case "Secret":
+		return clientset.CoreV1().Secrets(namespace).Get(context.TODO(), resourceName, metav1.GetOptions{})
+	case "Service":
+		return clientset.CoreV1().Services(namespace).Get(context.TODO(), resourceName, metav1.GetOptions{})
+	case "Deployment":
+		return clientset.AppsV1().Deployments(namespace).Get(context.TODO(), resourceName, metav1.GetOptions{})
+	case "HPA":
+		return clientset.AutoscalingV1().HorizontalPodAutoscalers(namespace).Get(context.TODO(), resourceName, metav1.GetOptions{})
+	case "Ingress":
+		return clientset.NetworkingV1beta1().Ingresses(namespace).Get(context.TODO(), resourceName, metav1.GetOptions{})
+	case "PDB":
+		return clientset.PolicyV1beta1().PodDisruptionBudgets(namespace).Get(context.TODO(), resourceName, metav1.GetOptions{})
+	case "Roles":
+		return clientset.RbacV1().Roles(namespace).Get(context.TODO(), resourceName, metav1.GetOptions{})
+	case "PVC":
+		return clientset.CoreV1().PersistentVolumeClaims(namespace).Get(context.TODO(), resourceName, metav1.GetOptions{})
+	case "StatefulSet":
+		return clientset.AppsV1().StatefulSets(namespace).Get(context.TODO(), resourceName, metav1.GetOptions{})
+	case "ServiceAccount":
+		return clientset.CoreV1().ServiceAccounts(namespace).Get(context.TODO(), resourceName, metav1.GetOptions{})
+	}
+	return nil, fmt.Errorf("resource type '%s' is not supported", resourceType)
 }
 
 func DeleteResource(diff []string, clientset kubernetes.Interface, namespace, resourceType string, noInteractive bool) ([]string, error) {
