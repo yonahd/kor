b7bc4e6af0a9a14606db3bd094dfaf114883d19e
Thuan Vo <thuan.votann@gmail.com>
2024-07-08 13:59:11 -0700

    feat: consider ingress and egress rules for networkpolicies (#310)
    * feat: consider ingress and egress rules for networkpolicies
    
    Signed-off-by: Thuan Vo <thuan.votann@gmail.com>
    
    * chore: rename utils functions
    
    * chore: use standard lib for slice ops
    
    * test: more test coverage
    
    * fixup: consider empty sources (select-all) as used
    
    * docs: update readme and bump chart appVersion
    
    * chore: bump chart version
    
    * test: add more test cases
    
    * chore: clean up
    
    * feat: consider ipBlock as used
    
    * chore: correct appVersion and clean up
    
    * chore: regenerate chart readme
    
    ---------
    
    Signed-off-by: Thuan Vo <thuan.votann@gmail.com>

--- pkg/kor/networkpolicies.go
+++ pkg/kor/networkpolicies.go
@@ -6,12 +6,114 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"slices"
 
+	v1 "k8s.io/api/core/v1"
+	networkingv1 "k8s.io/api/networking/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 
 	"github.com/yonahd/kor/pkg/filters"
 )
+
+const (
+	unusedLabelReason         = "Marked with unused label"
+	noPodAppliedReason        = "NetworkPolicy applies to 0 pods"
+	noPodAppliedByRulesReason = "NetworkPolicy Ingress and Egress rules apply to 0 pods"
+)
+
+func retrievePodsForSelector(clientset kubernetes.Interface, namespace string, selector *metav1.LabelSelector) ([]v1.Pod, error) {
+	labelSelector, err := metav1.LabelSelectorAsSelector(selector)
+	if err != nil {
+		return nil, err
+	}
+	podList, err := clientset.CoreV1().Pods(namespace).List(context.TODO(), metav1.ListOptions{
+		LabelSelector: labelSelector.String(),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return podList.Items, nil
+}
+
+func isAnyPodMatchedInSources(clientset kubernetes.Interface, sources []networkingv1.NetworkPolicyPeer) (bool, error) {
+	// If this field is empty or missing, this rule matches all pods
+	if len(sources) == 0 {
+		return true, nil
+	}
+
+	for _, netpolPeer := range sources {
+		// If ipBlock is specified, assume the source is in use
+		if netpolPeer.IPBlock != nil {
+			return true, nil
+		}
+
+		labelSelector, err := metav1.LabelSelectorAsSelector(netpolPeer.NamespaceSelector)
+		if err != nil {
+			return false, err
+		}
+
+		nsList, err := clientset.CoreV1().Namespaces().List(context.TODO(), metav1.ListOptions{
+			LabelSelector: labelSelector.String(),
+		})
+		if err != nil {
+			return false, err
+		}
+
+		for _, ns := range nsList.Items {
+			podList, err := retrievePodsForSelector(clientset, ns.Name, netpolPeer.PodSelector)
+			if err != nil {
+				return false, err
+			}
+
+			if len(podList) > 0 {
+				return true, nil
+			}
+		}
+	}
+
+	return false, nil
+}
+
+func isAnyIngressRuleUsed(clientset kubernetes.Interface, netpol networkingv1.NetworkPolicy) (bool, error) {
+	// Deny all ingress traffic
+	if len(netpol.Spec.Ingress) == 0 && slices.Contains(netpol.Spec.PolicyTypes, networkingv1.PolicyTypeIngress) {
+		return true, nil
+	}
+	for _, ingressRule := range netpol.Spec.Ingress {
+		podsMatched, err := isAnyPodMatchedInSources(clientset, ingressRule.From)
+		if err != nil {
+			return false, err
+		}
+
+		if podsMatched {
+			return true, nil
+		}
+	}
+
+	return false, nil
+}
+
+func isAnyEgressRuleUsed(clientset kubernetes.Interface, netpol networkingv1.NetworkPolicy) (bool, error) {
+	// Deny all egress traffic
+	if len(netpol.Spec.Egress) == 0 && slices.Contains(netpol.Spec.PolicyTypes, networkingv1.PolicyTypeEgress) {
+		return true, nil
+	}
+
+	for _, egressRule := range netpol.Spec.Egress {
+		podsMatched, err := isAnyPodMatchedInSources(clientset, egressRule.To)
+		if err != nil {
+			return false, err
+		}
+
+		if podsMatched {
+			return true, nil
+		}
+	}
+
+	return false, nil
+}
 
 func processNamespaceNetworkPolicies(clientset kubernetes.Interface, namespace string, filterOpts *filters.Options) ([]ResourceInfo, error) {
 	netpolList, err := clientset.NetworkingV1().NetworkPolicies(namespace).List(context.TODO(), metav1.ListOptions{LabelSelector: filterOpts.IncludeLabels})
@@ -27,27 +129,33 @@ func processNamespaceNetworkPolicies(clientset kubernetes.Interface, namespace s
 		}
 
 		if netpol.Labels["kor/used"] == "false" {
-			reason := "Marked with unused label"
-			unusedNetpols = append(unusedNetpols, ResourceInfo{Name: netpol.Name, Reason: reason})
+			unusedNetpols = append(unusedNetpols, ResourceInfo{Name: netpol.Name, Reason: unusedLabelReason})
 			continue
 		}
 
-		// retrieve pods selected by the NetworkPolicy
-		labelSelector, err := metav1.LabelSelectorAsSelector(&netpol.Spec.PodSelector)
-		if err != nil {
-			return nil, err
-		}
-		podList, err := clientset.CoreV1().Pods(namespace).List(context.TODO(), metav1.ListOptions{
-			LabelSelector: labelSelector.String(),
-		})
+		pods, err := retrievePodsForSelector(clientset, namespace, &netpol.Spec.PodSelector)
 		if err != nil {
 			return nil, err
 		}
 
-		if len(podList.Items) == 0 {
-			reason := "NetworkPolicy selects no pods"
-			unusedNetpols = append(unusedNetpols, ResourceInfo{Name: netpol.Name, Reason: reason})
+		if len(pods) == 0 {
+			unusedNetpols = append(unusedNetpols, ResourceInfo{Name: netpol.Name, Reason: noPodAppliedReason})
+			continue
 		}
+
+		if used, err := isAnyIngressRuleUsed(clientset, netpol); err != nil {
+			return nil, err
+		} else if used {
+			continue
+		}
+
+		if used, err := isAnyEgressRuleUsed(clientset, netpol); err != nil {
+			return nil, err
+		} else if used {
+			continue
+		}
+
+		unusedNetpols = append(unusedNetpols, ResourceInfo{Name: netpol.Name, Reason: noPodAppliedByRulesReason})
 	}
 
 	return unusedNetpols, nil
--- pkg/kor/networkpolicies_test.go
+++ pkg/kor/networkpolicies_test.go
@@ -19,56 +19,187 @@ import (
 func createTestNetworkPolicies(t *testing.T) *fake.Clientset {
 	clientset := fake.NewSimpleClientset()
 
-	_, err := clientset.CoreV1().Namespaces().Create(context.TODO(), &corev1.Namespace{
-		ObjectMeta: v1.ObjectMeta{Name: testNamespace},
-	}, v1.CreateOptions{})
+	testNamespace2 := "another-namespace"
+	namespaces := []string{testNamespace, testNamespace2}
 
-	if err != nil {
-		t.Fatalf("Error creating namespace %s: %v", testNamespace, err)
+	for _, ns := range namespaces {
+		_, err := clientset.CoreV1().Namespaces().Create(context.TODO(), &corev1.Namespace{
+			ObjectMeta: v1.ObjectMeta{Name: ns},
+		}, v1.CreateOptions{})
+
+		if err != nil {
+			t.Fatalf("Error creating namespace %s: %v", ns, err)
+		}
 	}
 
-	podLabels := map[string]string{
+	podLabels1 := map[string]string{
 		"app.kubernetes.io/name":    "my-app",
 		"app.kubernetes.io/version": "v1",
 		"product.my-org/name":       "my-app",
 	}
-	noMatchLabels := map[string]string{"app.kubernetes.io/version": "v2"}
+	podLabels2 := map[string]string{"app.kubernetes.io/version": "v2"}
 
 	pods := []*corev1.Pod{
-		CreateTestPod(testNamespace, "pod-1", "", nil, podLabels),
+		CreateTestPod(testNamespace, "pod-1", "", nil, podLabels1),
 		CreateTestPod(testNamespace, "pod-2", "", nil, AppLabels),
+		CreateTestPod(testNamespace2, "pod-1", "", nil, podLabels2),
+		CreateTestPod(testNamespace2, "pod-2", "", nil, AppLabels),
 	}
 
 	for _, pod := range pods {
-		_, err = clientset.CoreV1().Pods(testNamespace).Create(context.TODO(), pod, v1.CreateOptions{})
+		_, err := clientset.CoreV1().Pods(pod.Namespace).Create(context.TODO(), pod, v1.CreateOptions{})
 		if err != nil {
 			t.Fatalf("Error creating fake pod: %v", err)
 		}
 	}
 
 	netpols := []*networkingv1.NetworkPolicy{
-		// all pods are selected
-		CreateTestNetworkPolicy("netpol-1", testNamespace, v1.LabelSelector{}, AppLabels),
-		CreateTestNetworkPolicy("netpol-2", testNamespace, v1.LabelSelector{}, UsedLabels),
-		CreateTestNetworkPolicy("netpol-3", testNamespace, v1.LabelSelector{}, UnusedLabels),
-		// some pods are selected
-		CreateTestNetworkPolicy("netpol-4", testNamespace, *v1.SetAsLabelSelector(podLabels), AppLabels),
-		CreateTestNetworkPolicy("netpol-5", testNamespace, *v1.SetAsLabelSelector(podLabels), UnusedLabels),
-		CreateTestNetworkPolicy("netpol-6", testNamespace, *v1.SetAsLabelSelector(podLabels), UsedLabels),
+		// with kor labels
+		CreateTestNetworkPolicy("netpol-1", testNamespace, UsedLabels, v1.LabelSelector{}, nil, nil),
+		CreateTestNetworkPolicy("netpol-2", testNamespace, UnusedLabels, v1.LabelSelector{}, nil, nil),
+
+		// with pod selectors
 		// no pods are selected
-		CreateTestNetworkPolicy("netpol-7", testNamespace, *v1.SetAsLabelSelector(noMatchLabels), AppLabels),
-		CreateTestNetworkPolicy("netpol-8", testNamespace, *v1.SetAsLabelSelector(noMatchLabels), UnusedLabels),
-		CreateTestNetworkPolicy("netpol-9", testNamespace, *v1.SetAsLabelSelector(noMatchLabels), UsedLabels),
+		CreateTestNetworkPolicy("netpol-3", testNamespace, AppLabels, *v1.SetAsLabelSelector(podLabels2), nil, nil),
+
+		// with ingress/egress rules
+		// deny-all ingress
+		CreateTestNetworkPolicy("netpol-4", testNamespace, AppLabels, *v1.SetAsLabelSelector(podLabels1), nil, nil),
+		// allow-all ingress
+		CreateTestNetworkPolicy("netpol-5", testNamespace, AppLabels, *v1.SetAsLabelSelector(podLabels1), []networkingv1.NetworkPolicyIngressRule{{}}, nil),
+		// allow ingress to some pods
+		CreateTestNetworkPolicy("netpol-6", testNamespace, AppLabels, *v1.SetAsLabelSelector(podLabels1), []networkingv1.NetworkPolicyIngressRule{{
+			From: []networkingv1.NetworkPolicyPeer{
+				{
+					PodSelector: &v1.LabelSelector{
+						MatchLabels: podLabels2,
+					},
+				},
+			},
+		}}, nil),
+		// ingress matches 0 pods
+		CreateTestNetworkPolicy("netpol-7", testNamespace, AppLabels, *v1.SetAsLabelSelector(podLabels1), []networkingv1.NetworkPolicyIngressRule{{
+			From: []networkingv1.NetworkPolicyPeer{
+				{
+					PodSelector: &v1.LabelSelector{
+						MatchLabels: map[string]string{"product.my-org/name": "unknown"},
+					},
+				},
+			},
+		}}, nil),
+		// with ipBlock
+		CreateTestNetworkPolicy("netpol-8", testNamespace, AppLabels, *v1.SetAsLabelSelector(podLabels1), []networkingv1.NetworkPolicyIngressRule{{
+			From: []networkingv1.NetworkPolicyPeer{
+				{
+					IPBlock: &networkingv1.IPBlock{
+						CIDR: "172.17.0.0/16",
+					},
+				},
+			},
+		}}, nil),
 	}
 
+	netpol9 := CreateTestNetworkPolicy("netpol-9", testNamespace, AppLabels, *v1.SetAsLabelSelector(podLabels1), nil, []networkingv1.NetworkPolicyEgressRule{{
+		To: []networkingv1.NetworkPolicyPeer{
+			{
+				PodSelector: &v1.LabelSelector{
+					MatchLabels: map[string]string{"product.my-org/name": "unknown"},
+				},
+			},
+		},
+	}}) // egress only - matches 0 pods
+	netpol9.Spec.PolicyTypes = []networkingv1.PolicyType{networkingv1.PolicyTypeEgress}
+	netpols = append(netpols, netpol9)
+
 	for _, netpol := range netpols {
-		_, err = clientset.NetworkingV1().NetworkPolicies(netpol.Namespace).Create(context.TODO(), netpol, v1.CreateOptions{})
+		_, err := clientset.NetworkingV1().NetworkPolicies(netpol.Namespace).Create(context.TODO(), netpol, v1.CreateOptions{})
 		if err != nil {
 			t.Fatalf("Error creating fake networkpolicy: %v", err)
 		}
 	}
 
 	return clientset
+}
+
+func TestRetrievePodsForSelector(t *testing.T) {
+	clientset := createTestNetworkPolicies(t)
+
+	selector := &v1.LabelSelector{
+		MatchLabels: map[string]string{
+			"app.kubernetes.io/version": "v1",
+		},
+	}
+	pods, err := retrievePodsForSelector(clientset, testNamespace, selector)
+	if err != nil {
+		t.Errorf("Error retrieving pods for selector %v: %v", selector, err)
+	}
+
+	expectedPods := []string{
+		"pod-1",
+	}
+
+	if len(pods) != len(expectedPods) {
+		t.Errorf("Expected %d pods, got %d", len(expectedPods), len(pods))
+	}
+
+	for i, pod := range pods {
+		if pod.Name != expectedPods[i] {
+			t.Errorf("Expected pod %s, got %v", expectedPods[i], pod)
+		}
+	}
+}
+
+func TestIsAnyPodMatchedInSources(t *testing.T) {
+	clientset := createTestNetworkPolicies(t)
+
+	sources := []networkingv1.NetworkPolicyPeer{
+		{
+			PodSelector: &v1.LabelSelector{
+				MatchLabels: map[string]string{
+					"app.kubernetes.io/version": "v1",
+				},
+			},
+		},
+	}
+
+	matched, err := isAnyPodMatchedInSources(clientset, sources)
+	if err != nil {
+		t.Errorf("Error checking if sources match any pods: %v", err)
+	}
+
+	if !matched {
+		t.Error("Expected matching pods, got none")
+	}
+}
+
+func TestIsAnyIngressRuleUsed(t *testing.T) {
+	clientset := createTestNetworkPolicies(t)
+
+	netpol := CreateTestNetworkPolicy("netpol-0", testNamespace, AppLabels, v1.LabelSelector{}, nil, nil)
+
+	used, err := isAnyIngressRuleUsed(clientset, *netpol)
+	if err != nil {
+		t.Errorf("Error checking if any ingress rule is used: %v", err)
+	}
+
+	if !used {
+		t.Error("Expected ingress rules in use, got none")
+	}
+}
+
+func TestIsAnyEgressRuleUsed(t *testing.T) {
+	clientset := createTestNetworkPolicies(t)
+
+	netpol := CreateTestNetworkPolicy("netpol-0", testNamespace, AppLabels, v1.LabelSelector{}, nil, nil)
+
+	used, err := isAnyEgressRuleUsed(clientset, *netpol)
+	if err != nil {
+		t.Errorf("Error checking if any egress rule is used: %v", err)
+	}
+
+	if used {
+		t.Error("Expected ingress rules not in use, got used rules")
+	}
 }
 
 func TestProcessNamespaceNetworkPolicies(t *testing.T) {
@@ -80,10 +211,10 @@ func TestProcessNamespaceNetworkPolicies(t *testing.T) {
 	}
 
 	expectedUnusedNetpols := []string{
+		"netpol-2",
 		"netpol-3",
-		"netpol-5",
 		"netpol-7",
-		"netpol-8",
+		"netpol-9",
 	}
 
 	if len(unusedNetpols) != len(expectedUnusedNetpols) {
@@ -117,10 +248,10 @@ func TestGetUnusedNetworkPolicies(t *testing.T) {
 	expectedOutput := map[string]map[string][]string{
 		testNamespace: {
 			"NetworkPolicy": []string{
+				"netpol-2",
 				"netpol-3",
-				"netpol-5",
 				"netpol-7",
-				"netpol-8",
+				"netpol-9",
 			},
 		},
 	}
