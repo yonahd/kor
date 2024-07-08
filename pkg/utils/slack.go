22b6800b7873d054a467243c15a7eb0a34892570
Yonah Dissen <yonahdissen@Yonahs-MacBook-Pro.local>
2024-07-08 21:14:34 +0300

    chore: move slack to utils directory

--- pkg/utils/slack.go
+++ pkg/utils/slack.go
@@ -9,20 +9,22 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+
+	"github.com/yonahd/kor/pkg/kor"
 )
 
 type SendMessageToSlack interface {
-	SendToSlack(opts Opts, outputBuffer string) error
+	SendToSlack(opts kor.Opts, outputBuffer string) error
 }
 
 type SlackMessage struct {
 }
 
-func SendToSlack(sm SendMessageToSlack, opts Opts, outputBuffer string) error {
+func SendToSlack(sm SendMessageToSlack, opts kor.Opts, outputBuffer string) error {
 	return sm.SendToSlack(opts, outputBuffer)
 }
 
-func (sm SlackMessage) SendToSlack(opts Opts, outputBuffer string) error {
+func (sm SlackMessage) SendToSlack(opts kor.Opts, outputBuffer string) error {
 	if opts.WebhookURL != "" {
 		payload := []byte(`{"text": "` + outputBuffer + `"}`)
 		_, err := http.Post(opts.WebhookURL, "application/json", bytes.NewBuffer(payload))
