ccdd7f8b24ed042229f8469b9d038e3ae7e7cbda
Eriks Zelenka <isindir@users.sf.net>
2025-03-09 07:27:24 +0000

    add TestGetUnusedNamespaces

---
+++ pkg/kor/namespaces_GetUnusedNamespaces_test.go
@@ -0,0 +1,118 @@
+package kor
+
+import (
+	"context"
+	"testing"
+
+	eventsv1 "k8s.io/api/events/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	discoveryfake "k8s.io/client-go/discovery/fake"
+	dynamicfake "k8s.io/client-go/dynamic/fake"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/kubernetes/fake"
+
+	"github.com/yonahd/kor/pkg/common"
+	"github.com/yonahd/kor/pkg/filters"
+)
+
+type GetFakeClientInterfacesForGetUnusedNamespacesTestFunc func(ctx context.Context, t *testing.T) (kubernetes.Interface, *dynamicfake.FakeDynamicClient)
+
+func defineNewTypeEventObject(ns, name string) *eventsv1.Event {
+	return &eventsv1.Event{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: ns,
+		},
+		ReportingController: "some-controller",
+		Type:                "Warning",
+	}
+}
+
+func createEmptyNamespace(ctx context.Context, t *testing.T) (kubernetes.Interface, *dynamicfake.FakeDynamicClient) {
+	realClientset := fake.NewSimpleClientset()
+	fakeDisc := &fakeHappyDiscovery{discoveryfake.FakeDiscovery{Fake: &realClientset.Fake}}
+	clientset := &fakeClientset{Interface: realClientset, discovery: fakeDisc}
+	scheme := getNamespaceTestSchema(t)
+
+	ns1 := "empty-namespace"
+	namespace1 := defineNamespaceObject(ns1)
+	_, err := clientset.CoreV1().Namespaces().Create(ctx, namespace1, metav1.CreateOptions{})
+	if err != nil {
+		t.Fatalf("Failed to create test namespace: %v", err)
+	}
+
+	ns2 := "test-namespace"
+	namespace2 := defineNamespaceObject(ns2)
+	_, err = clientset.CoreV1().Namespaces().Create(ctx, namespace2, metav1.CreateOptions{})
+	if err != nil {
+		t.Fatalf("Failed to create test namespace: %v", err)
+	}
+
+	evtName := "some-random-event"
+	newEventType := defineNewTypeEventObject(ns1, evtName)
+	_, err = clientset.EventsV1().Events(ns1).Create(ctx, newEventType, metav1.CreateOptions{})
+	if err != nil {
+		t.Fatalf("Failed to create test event of group events.k8s.io: %v", err)
+	}
+
+	listKinds := map[schema.GroupVersionResource]string{
+		{Group: "apps", Version: "v1", Resource: "deployments"}:     "DeploymentList",
+		{Group: "", Version: "v1", Resource: "namespaces"}:          "NamespaceList",
+		{Group: "events.k8s.io", Version: "v1", Resource: "events"}: "EventList",
+		{Group: "", Version: "v1", Resource: "events"}:              "EventList",
+	}
+	dynamicClient := dynamicfake.NewSimpleDynamicClientWithCustomListKinds(scheme, listKinds, namespace1, namespace2)
+
+	return clientset, dynamicClient
+}
+
+func TestGetUnusedNamespaces(t *testing.T) {
+	tests := []struct {
+		name string
+
+		getClientsFunc GetFakeClientInterfacesForGetUnusedNamespacesTestFunc
+
+		filterOpts *filters.Options
+
+		expectedOutput string
+		expectedError  bool
+	}{
+		{
+			name:           "empty namespace, only event exists",
+			getClientsFunc: createEmptyNamespace,
+			filterOpts:     &filters.Options{},
+			expectedOutput: `{
+  "": {
+    "Namespace": [
+      "empty-namespace",
+      "test-namespace"
+    ]
+  }
+}`,
+			expectedError: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctx := context.TODO()
+			opts := common.Opts{
+				WebhookURL:    "",
+				Channel:       "",
+				Token:         "",
+				DeleteFlag:    false,
+				NoInteractive: true,
+				GroupBy:       "namespace",
+			}
+
+			clientset, dynamicClient := tt.getClientsFunc(ctx, t)
+			got, err := GetUnusedNamespaces(ctx, tt.filterOpts, clientset, dynamicClient, "json", opts)
+			if (err != nil) != tt.expectedError {
+				t.Errorf("GetUnusedNamespaces() = expected error: %t, got: '%v'", tt.expectedError, err)
+			}
+			if got != tt.expectedOutput {
+				t.Errorf("GetUnusedNamespaces() = got:\n'%s'\nwant:\n'%s'", got, tt.expectedOutput)
+			}
+		})
+	}
+}
