973c55215d3e9fd363580d65e5cb20dec3534396
Yonah Dissen <47282577+yonahd@users.noreply.github.com>
2026-01-12 08:17:32 +0200

    chore: update go to 1.25 (#543)
    * chore: update go to 1.25
    
    * chore: update go to 1.25
    
    * add explicit version
    
    * add explicit version
    
    * add explicit version
    
    * add explicit version
    
    * add explicit version
    
    * Fix ConfigMap reference to use Name instead of ObjectMeta.Name
    
    * Fix ConfigMap reference in configmaps_test.go
    
    * Fix secret reference in secrets_test.go
    
    * Fix image pull secret references in tests
    
    ---------
    
    Co-authored-by: Yonah Dissen <yonahdissen@Yonahs-MacBook-Pro.local>

--- pkg/kor/secrets_test.go
+++ pkg/kor/secrets_test.go
@@ -55,7 +55,7 @@ func createTestSecrets(t *testing.T) *fake.Clientset {
 			Env: []corev1.EnvVar{
 				{
 					Name:      "ENV_VAR_1",
-					ValueFrom: &corev1.EnvVarSource{SecretKeyRef: &corev1.SecretKeySelector{LocalObjectReference: corev1.LocalObjectReference{Name: secret1.ObjectMeta.Name}}},
+					ValueFrom: &corev1.EnvVarSource{SecretKeyRef: &corev1.SecretKeySelector{LocalObjectReference: corev1.LocalObjectReference{Name: secret1.Name}}},
 				},
 			},
 		},
@@ -65,7 +65,7 @@ func createTestSecrets(t *testing.T) *fake.Clientset {
 	pod4.Spec.Containers = []corev1.Container{
 		{
 			EnvFrom: []corev1.EnvFromSource{
-				{SecretRef: &corev1.SecretEnvSource{LocalObjectReference: corev1.LocalObjectReference{Name: secret1.ObjectMeta.Name}}},
+				{SecretRef: &corev1.SecretEnvSource{LocalObjectReference: corev1.LocalObjectReference{Name: secret1.Name}}},
 			},
 		},
 	}
@@ -76,7 +76,7 @@ func createTestSecrets(t *testing.T) *fake.Clientset {
 			Env: []corev1.EnvVar{
 				{
 					Name:      "INIT_ENV_VAR_1",
-					ValueFrom: &corev1.EnvVarSource{SecretKeyRef: &corev1.SecretKeySelector{LocalObjectReference: corev1.LocalObjectReference{Name: secret1.ObjectMeta.Name}}},
+					ValueFrom: &corev1.EnvVarSource{SecretKeyRef: &corev1.SecretKeySelector{LocalObjectReference: corev1.LocalObjectReference{Name: secret1.Name}}},
 				},
 			},
 		},
@@ -84,15 +84,15 @@ func createTestSecrets(t *testing.T) *fake.Clientset {
 
 	pod6 := CreateTestPod(testNamespace, "pod-6", "", nil, AppLabels)
 	pod6.Spec.ImagePullSecrets = []corev1.LocalObjectReference{
-		{Name: secret1.ObjectMeta.Name},
-		{Name: secret2.ObjectMeta.Name},
+		{Name: secret1.Name},
+		{Name: secret2.Name},
 	}
 
 	pod7 := CreateTestPod(testNamespace, "pod-7", "", nil, AppLabels)
 	pod7.Spec.InitContainers = []corev1.Container{
 		{
 			EnvFrom: []corev1.EnvFromSource{
-				{SecretRef: &corev1.SecretEnvSource{LocalObjectReference: corev1.LocalObjectReference{Name: secret6.ObjectMeta.Name}}},
+				{SecretRef: &corev1.SecretEnvSource{LocalObjectReference: corev1.LocalObjectReference{Name: secret6.Name}}},
 			},
 		},
 	}
